b0667fab423bb04627dc47803242268810b908db
tharnyelilvannan <tharny2005@outlook.com>
2024-10-25 17:07:14 -0400

    Update mistakes.py program.

--- mistakes.py
+++ mistakes.py
@@ -1,5 +1,33 @@
 # Author: Tharny Elilvannan
 # Last Updated: October 25, 2024
 # Purpose: Ask students to look through code and fix mistakes.
+#          There will be syntax and semantic errors.
 
+
+# Question 1
 int button = "1"
+
+
+# Question 2
+for i in 5:
+print(i)
+
+
+# Question 3
+c = 22
+while (c > 0)
+    print(c)
+
+
+# Question 4
+b = 54
+while (b < 0)
+    print(b)
+    b = b + 1
+
+
+# Question 5
+d = 99
+while (d > 0):
+    print(d)
+    d = d -1
